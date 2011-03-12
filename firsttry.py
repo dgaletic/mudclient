83954be62b2a3b4c558241ebf59c7db86850197d
Dinko <dgaletic@everflame-mint.(none)>
2011-03-12 01:02:54 +0100

    Added the first version of the MUD client!

---
+++ firsttry.py
@@ -0,0 +1,35 @@
+def inputter(conn):
+    text = ""
+    while text != "quit":
+        text = raw_input()
+        conn.write(text + "\n")
+        
+# Start as a new thread!
+def outputter(conn):
+    import time
+    try:
+        while(True):
+            text = conn.read_very_eager()
+            if text != "":
+                # Stop print from automatically printing a newline
+                print text
+                text = ""
+            time.sleep(0.1)
+    except EOFError:
+        print "So this is goodbye..."
+        return
+
+import telnetlib 
+import thread
+
+session = telnetlib.Telnet()
+
+host = "discworld.atuin.net"
+port = 4242
+
+session.open(host, port)
+
+
+thread.start_new_thread(outputter, (session,))
+
+inputter(session)    
