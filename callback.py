4e19b74b423b76235d4dc4e22ff084023113236e
dgaletic <dinko.galetic@gmail.com>
2011-03-18 15:34:43 +0100

    Quick save of some changes before going home. Working on compression at the moment, and merged the two files together.

--- callback.py
+++ callback.py
@@ -3,6 +3,7 @@ COMPRESS2 = chr(86)
 
 import zlib
 import telnetlib
+import time
 
 compressor = None
 
@@ -16,25 +17,23 @@ def tcallback(tsocket, tcommand, toption):
             # respond to the server that we accept
             # TODO: Is this ok? Server doesn't respond to it by
             # sending IAC SB COMPRESS2 IAC SE
-            tsocket.send(telnetlib.IAC)
-            tsocket.send(telnetlib.DO)
-            tsocket.send(COMPRESS2)
-            tsocket.send("\n")
+            tsocket.send(telnetlib.IAC + telnetlib.DO + COMPRESS2   )
 
 # the server may begin compression at any time by sending a 
 # IAC SB COMPRESS2 IAC SE sequence, immediately followed by the start # of the compressed stream. 
-    if tcommand == telnetlib.SB:
+    elif tcommand == telnetlib.SB:
+        print "IAC SB"
         if toption == COMPRESS2:
-            # All right, we're compressing! So:
-            # the next thing we receive will be compressed.
-            # set the global compression object
+            # After IAC DO COMPRESS2, server responds by
+            # 250 0 240 0 instead of 250 86 240 0(SB COMPRESS2) 
             print "Server says: Next thing be compressed."
-            compressor = zlib.compressobj()
+            # compressor = zlib.compressobj()
             # Actually the next thing will be IAC SE, but 
             # that is caught by the callback, not the displaying func.
             pass
-    if tcommand == telnetlib.SE:
-            print "IAC SE"
+    elif tcommand == telnetlib.SE:
+            print "IAC SE; (hackish) starting compression now"
+            compressor = zlib.compressobj()
             # Nothing for now.
             pass
 
--- firsttry.py
+++ firsttry.py
@@ -2,15 +2,47 @@
 
 import telnetlib 
 import thread
-from callback import *
+import zlib
+import time
+
+COMPRESS = chr(85)
+COMPRESS2 = chr(86)
+
+compressor = None
+session = telnetlib.Telnet()
+
+def tcallback(tsocket, tcommand, toption):
+    print "WE GOT SOMETHIN"
+    print "Command: ", ord(tcommand)
+    print "Option: ", ord(toption)
+    if tcommand == telnetlib.WILL:
+        if toption == COMPRESS2:
+            print "We got a COMPRESS2 request from the server"
+            # respond to the server that we accept
+            tsocket.send(telnetlib.IAC + telnetlib.DO + COMPRESS2)
+
+    elif tcommand == telnetlib.SB:
+        print "IAC SB, I wonder what we're negotiating"
+        # we'll find out when we get IAC SE and read the sb_data!
+
+    elif tcommand == telnetlib.SE:
+            print "IAC SE"
+            # Now, call the Telnet.read_sb_data() to figure what happened.
+            sb_data = session.read_sb_data()
+            print "sb_data:", sb_data
+            # if that data is COMPRESS2, encrypted stream immediately follows
+            if sb_data == COMPRESS2:
+                compressor = zlib.decompressobj()
+
+            pass
 
 def inputter(conn):
     text = ""
     while text != "quit":
-        text = raw_input()
-        conn.write(text + "\n")
+        text = raw_input() + "\n"
+        conn.write(text)
         
-# Start as a new thread!
+# Start this as a new thread!
 def outputter(conn):
     import time
     try:
@@ -19,6 +51,8 @@ def outputter(conn):
             if text:
                 # Any way to stop print from printing anything
                 # after it prints text?
+                if compressor:
+                    text = compressor.decompress(text)
                 print text,
                 text = None 
             time.sleep(0.1)
@@ -29,9 +63,8 @@ def outputter(conn):
 host = "discworld.atuin.net"
 port = 4242
 
-session = telnetlib.Telnet()
 session.set_option_negotiation_callback(tcallback)
 session.open(host, port)
 
-thid = thread.start_new_thread(outputter, (session,))
-inputter(session)    
+thid = thread.start_new_thread(inputter, (session,))
+outputter(session)    
